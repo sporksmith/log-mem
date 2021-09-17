447b89fc0100be20e12820f4e018771c0c7b8e40
Jim Newsome <jnewsome@torproject.org>
2021-09-17 15:25:00 -0500

    kind of works, but v large files

--- log_smaps.py
+++ log_smaps.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
-import sys
+import csv
 import os
 import subprocess
+import sys
+import time
 
 from collections import defaultdict
 
 
-def mappings_for_pid(pid):
-    pmap = subprocess.run(
-            ['pmap', '-XX', str(pid)],
-            text=True,
-            check=True,
-            capture_output=True)
+def get_mappings_for_pid(pid):
+    try:
+        pmap = subprocess.run(
+                ['pmap', '-XX', str(pid)],
+                text=True,
+                check=True,
+                capture_output=True)
+    except subprocess.CalledProcessError:
+        return {}
+
     lines = pmap.stdout.splitlines()
     cmd = ' '.join(lines[0].split()[1:])
     labels = lines[1].split()
     pss_idx = labels.index('Pss')
     rss_idx = labels.index('Rss')
-
-    print(f'pss_idx: {pss_idx}')
-    print(f'rss_idx: {rss_idx}')
 
     mappings = defaultdict(lambda: {'pss': 0, 'rss': 0})
     for mapline in lines[2:-2]:
@@ -37,16 +40,16 @@ def mappings_for_pid(pid):
         pss = int(mapline[pss_idx])
         rss = int(mapline[rss_idx])
 
-        print(f'parsed pss:{pss} rss:{rss} name:{name}')
+        #print(f'parsed pss:{pss} rss:{rss} name:{name}')
 
         mappings[(cmd, name)]['pss'] += pss
         mappings[(cmd, name)]['rss'] += rss
 
     return mappings
 
-def mappings_for_pids(pids):
+def get_mappings_for_pids(pids):
     # sequence of map of (cmd, name) to {'rss': x, 'pss': y}
-    all_mappings = map(mappings_for_pid, pids)
+    all_mappings = map(get_mappings_for_pid, pids)
 
     # merged map of (cmd, name) to {'rss': x, 'pss': y}
     merged_mappings = defaultdict(lambda: {'pss': 0, 'rss': 0})
@@ -57,6 +60,31 @@ def mappings_for_pids(pids):
 
     return merged_mappings
 
+def get_pids():
+    pmap = subprocess.run(
+            ['ps', 'x', '-o', 'pid'],
+            text=True,
+            check=True,
+            capture_output=True)
+    return map(int, pmap.stdout.splitlines()[1:])
+
+def log_current_usage(dirname):
+    pids = get_pids()
+    mappings = get_mappings_for_pids(get_pids())
+    unix_seconds = int(time.time_ns()/1000000000)
+
+    f = open(os.path.join(dirname, str(unix_seconds)), 'w')
+    csv_writer = csv.writer(f)
+    for ((cmd, name), mapping) in mappings.items():
+        csv_writer.writerow([mapping['pss'],mapping['rss'],cmd,name])
+    f.close()
+
 if __name__ == '__main__':
-    sleep_time = int(sys.argv[1])
-    print('hello')
+    dirname = sys.argv[1]
+    sleep_time = int(sys.argv[2])
+
+    os.mkdir(dirname)
+
+    while True:
+        log_current_usage(dirname)
+        time.sleep(sleep_time)
