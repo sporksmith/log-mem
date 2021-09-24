2e58d9a78cc28e07784bb5202cf76c637d37a7c5
Jim Newsome <jnewsome@torproject.org>
2021-09-24 17:06:47 -0500

    Write csv to stdout instead of individual files

--- log_smaps.py
+++ log_smaps.py
@@ -1,90 +1,105 @@
 #!/usr/bin/env python3
 
 import csv
+import itertools
 import os
 import subprocess
 import sys
 import time
 
 from collections import defaultdict
+from collections import namedtuple
+from typing import NamedTuple
 
+class MemMapping(NamedTuple):
+    time: int
+    cmd: str
+    name: str
+    pss: int
+    rss: int
 
-def get_mappings_for_pid(pid):
+def merge(mappings):
+    merged_mappings = []
+    key_fn = lambda m: (m.time, m.cmd, m.name)
+    mappings = mappings[:]
+    mappings.sort(key=key_fn)
+    for (mtime, cmd, name), group in itertools.groupby(mappings, key_fn):
+        #group = list(group)
+        #print(len(group))
+        merged_mappings.append(
+                MemMapping(
+                    time=mtime,
+                    cmd=cmd,
+                    name=name,
+                    pss=sum([m.pss for m in group]),
+                    rss=sum([m.rss for m in group])))
+    return merged_mappings
+
+def get_mappings_for_pid(pid, time):
     try:
         pmap = subprocess.run(
                 ['pmap', '-XX', str(pid)],
-                text=True,
+                universal_newlines=True,
                 check=True,
-                capture_output=True)
+                stdout=subprocess.PIPE)
     except subprocess.CalledProcessError:
-        return {}
+        return []
 
     lines = pmap.stdout.splitlines()
     cmd = ' '.join(lines[0].split()[1:])
     labels = lines[1].split()
     pss_idx = labels.index('Pss')
     rss_idx = labels.index('Rss')
+    flags_and_mapping_idx = labels.index('VmFlagsMapping')
 
-    mappings = defaultdict(lambda: {'pss': 0, 'rss': 0})
+    mappings = []
     for mapline in lines[2:-2]:
-        mapline = mapline.split()
-
-        # There's not a great way to distinguish the (variable number of) flags
-        # from the mapping name, which may not be present. Assume that if the
-        # last field is 2-long, then it's actually one of the vmflags and there
-        # is no mapping name.
-        name = mapline[-1]
-        if len(name) == 2:
-            name = 'anon'
+        # Split, but careful not to split last field, VmFlagsMapping
+        mapline = mapline.split(None, len(labels) - 1)
 
         pss = int(mapline[pss_idx])
         rss = int(mapline[rss_idx])
 
-        #print(f'parsed pss:{pss} rss:{rss} name:{name}')
+        flags_and_mapping = mapline[flags_and_mapping_idx]
+        name = mapline[flags_and_mapping_idx].split('  ')[1]
 
-        mappings[(cmd, name)]['pss'] += pss
-        mappings[(cmd, name)]['rss'] += rss
+        mapping = MemMapping(time=time, cmd=cmd, name=name, pss= pss, rss= rss)
+        mappings.append(mapping)
 
-    return mappings
+    return merge(mappings)
 
 def get_mappings_for_pids(pids):
-    # sequence of map of (cmd, name) to {'rss': x, 'pss': y}
-    all_mappings = map(get_mappings_for_pid, pids)
+    unix_seconds = int(time.time())
 
-    # merged map of (cmd, name) to {'rss': x, 'pss': y}
-    merged_mappings = defaultdict(lambda: {'pss': 0, 'rss': 0})
-    for mappings in all_mappings:
-        for ((cmd, name), mapping) in mappings.items():
-            merged_mappings[(cmd, name)]['pss'] += mapping['pss']
-            merged_mappings[(cmd, name)]['rss'] += mapping['rss']
+    all_mappings = map(lambda pid: get_mappings_for_pid(pid, unix_seconds), pids)
 
-    return merged_mappings
+    # flatten
+    all_mappings = [val for sublist in all_mappings for val in sublist]
+
+    return merge(all_mappings)
 
 def get_pids():
     pmap = subprocess.run(
             ['ps', 'x', '-o', 'pid'],
-            text=True,
+            universal_newlines=True,
             check=True,
-            capture_output=True)
+            stdout=subprocess.PIPE)
     return map(int, pmap.stdout.splitlines()[1:])
 
-def log_current_usage(dirname):
+def log_current_usage():
     pids = get_pids()
     mappings = get_mappings_for_pids(get_pids())
-    unix_seconds = int(time.time_ns()/1000000000)
 
-    f = open(os.path.join(dirname, str(unix_seconds)), 'w')
-    csv_writer = csv.writer(f)
-    for ((cmd, name), mapping) in mappings.items():
-        csv_writer.writerow([mapping['pss'],mapping['rss'],cmd,name])
-    f.close()
+    #csv_writer = csv.writer(sys.stdout)
+    csv_writer = csv.DictWriter(sys.stdout, ['time', 'cmd', 'name', 'pss', 'rss'])
+    csv_writer.writeheader()
+    for mapping in mappings:
+        csv_writer.writerow(mapping._asdict())
+    sys.stdout.flush()
 
 if __name__ == '__main__':
-    dirname = sys.argv[1]
-    sleep_time = int(sys.argv[2])
-
-    os.mkdir(dirname)
+    sleep_time = int(sys.argv[1])
 
     while True:
-        log_current_usage(dirname)
+        log_current_usage()
         time.sleep(sleep_time)
